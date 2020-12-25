c415976113c59c9e04fe20fa592ccb9da2b70ae4
Ankit Jaiswal <ankit.jaiswal02@ad.infosys.com>
2020-12-25 22:49:20 +0530

    first commit

---
+++ Raju.java
@@ -0,0 +1,10 @@
+public class Raju {
+    public static void main(String args[]){
+        //System.out.println("Ankit");
+        int a=10, b=20, c=30,result;
+
+        result=((a>b)?((a>c)?a:c):(b>c)?b:c);
+        System.out.println(result);
+
+    }
+}
